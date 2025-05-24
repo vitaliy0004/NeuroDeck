8680ec24b69d7a6876fa7f1dd4f1e8fd6e417340
vitaliy0004 <dymovvs11@gmail.com>
2025-05-24 20:42:48 +0300

    n

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding),
